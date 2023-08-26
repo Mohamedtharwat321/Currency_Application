afcb0efa8520b7ff09c0b2e55dc35d4e5438f3ce
hishamkamell <140950880+hishamkamell@users.noreply.github.com>
2023-08-26 14:46:40 +0300

    aa

--- src/app/Components/live-exec/live-exec.component.ts
+++ src/app/Components/live-exec/live-exec.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { GetcurrencyService } from 'src/app/Services/getcurrency.service';
 
 @Component({
   selector: 'app-live-exec',
   templateUrl: './live-exec.component.html',
   styleUrls: ['./live-exec.component.scss']
 })
-export class LiveExecComponent {
-
+export class LiveExecComponent implements OnInit{
+  
+    public currencyList = [];
+    
+    constructor(
+      private currencyservice: GetcurrencyService,
+    ) {}
+    ngOnInit(): void {
+      this.getCurrencyList();
+    }
+    getCurrencyList() {
+      this.currencyservice.getCurrency().subscribe((result: any) => {
+        this.currencyList = result;
+        console.log(result);
+      });
+    }
 }
--- src/app/Components/popup/popup.component.ts
+++ src/app/Components/popup/popup.component.ts
@@ -2,12 +2,6 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { PopupCardComponent } from './popup-card/popup-card/popup-card.component';
 import { GetcurrencyService } from 'src/app/Services/getcurrency.service';
 
-
-export interface favData {
-  icon: string;
-  cur: string;
-} 
-
 @Component({
   selector: 'app-popup',
   templateUrl: './popup.component.html',
@@ -20,13 +14,13 @@ export class PopupComponent implements OnInit {
     private currencyservice: GetcurrencyService,
   ) {}
   ngOnInit(): void {
-    this.getCurrency();
+    this.getCurrencyList();
   }
-  getCurrency(){
-    this.currencyservice.getCurrency().subscribe((result :any)=>{
+  getCurrencyList() {
+    this.currencyservice.getCurrency().subscribe((result: any) => {
       this.currencyList = result;
       console.log(result);
-    })
+    });
   }
 }
 
